e38dd7763889fa5896943ee5ed50177aeb965cb3
vitaliylikhitskiy <vitaliy.likhitskyi@mev.com>
2025-05-17 20:53:56 +0300

    update application to listen on all network interfaces

--- apps/rent-api/src/main.ts
+++ apps/rent-api/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
